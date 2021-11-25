1cec33a0a966f842eb67ca83827bf77de92d19dc
Igor Mo <motrik@gmail.com>
2021-11-26 00:08:33 +0200

    heroku

--- commerce/settings.py
+++ commerce/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-import os
+STATIC_URL = '/static/'
+
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
+  os.path.join(SITE_ROOT, '../auctions/static/'),
 )
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
