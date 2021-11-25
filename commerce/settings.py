7f4f6010c140f25a46dd456b67102ed5244cedf7
Igor <motrik@gmail.com>
2021-11-25 18:19:27 +0200

    fix settings

--- commerce/settings.py
+++ commerce/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 STATIC_URL = '/static/'
