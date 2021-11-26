061bfe0081d2faa9e6b0fafffc39c9d26b535ec0
Igor Mo <motrik@gmail.com>
2021-11-26 08:05:27 +0200

    fixes

--- commerce/settings.py
+++ commerce/settings.py
@@ -118,10 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 #SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 #STATICFILES_DIRS = (
 #  os.path.join(SITE_ROOT, '../auctions/static/'),
 #)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
